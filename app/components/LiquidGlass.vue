11ab2266d0d5563d9c8abb752392f0e06a63dbc7
Chonchanat Tubtiang <chonchanat.t@kisracorp.com>
2025-10-17 11:22:39 +0700

    update design setting page, create liquidGlass component (test)

---
+++ app/components/LiquidGlass.vue
@@ -0,0 +1,72 @@
+<!-- develope -->
+<template>
+  <div class="card flex gap-4" draggable="true">
+    <filter id="displacementFilter">
+
+      <Icon name="logos:facebook" class="w-12 h-12" />
+      <Icon name="flat-color-icons:google" class="w-12 h-12" />
+      <Icon name="logos:youtube-icon" class="w-12 h-12" />
+    </filter>
+  </div>
+
+  <!-- <div class="card" draggable="true">
+        <slot></slot>
+    </div>
+
+    <svg style="display:none;">
+        <filter id="displacementFilter">
+            <feTurbulence type="turbulence" 
+                baseFrequency="0.01" 
+                numOctaves="2" 
+                result="turbulence" />
+    
+            <feDisplacementMap in="SourceGraphic"
+                in2="turbulence"    
+                            scale="200" xChannelSelector="R" yChannelSelector="G" />
+        </filter>
+    </svg> -->
+</template>
+
+<script setup>
+
+
+</script>
+
+<style scoped>
+.card {
+  width: fit-content;
+  flex-wrap: wrap;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 16px;
+  overflow: hidden;
+  border-radius: inherit;
+  transition: opacity 0.26s ease-out;
+  border-radius: 28px;
+  filter: drop-shadow(-8px -10px 46px #0000005f);
+  backdrop-filter: brightness(1.1) blur(2px) url(#displacementFilter);
+}
+
+.card::before {
+  content: '';
+  position: absolute;
+  inset: 0;
+  z-index: 0;
+  overflow: hidden;
+  border-radius: 28px;
+  -webkit-box-shadow: inset 2px 2px 0px -2px rgba(255, 255, 255, 0.7),
+    inset 0 0 3px 1px rgba(255, 255, 255, 0.7);
+  box-shadow: inset 6px 6px 0px -6px rgba(255, 255, 255, 0.7),
+    inset 0 0 8px 1px rgba(255, 255, 255, 0.7);
+}
+
+.liquid-glass-button {
+  background: rgba(255, 255, 255, 0.15);
+  backdrop-filter: url(#liquid-glass-filter) blur(10px);
+  /* Apply SVG filter and then blur */
+  -webkit-backdrop-filter: url(#liquid-glass-filter) blur(10px);
+  /* For Safari */
+  /* Other styling as in the basic glassmorphism example */
+}
+</style>
--- app/pages/(profile)/setting.vue
+++ app/pages/(profile)/setting.vue
@@ -1,34 +1,34 @@
 <template>
   <div class="mb-4 mx-4 sm:mx-16">
     <div class="p-4 sm:p-8 bg-[#1F272F] rounded-[20px] space-y-8">
-      <h3 class="text-[20px] sm:text-[32px] font-bold">Setting</h3>
+      <h3 class="text-[18px] sm:text-[32px] font-bold">Setting</h3>
 
       <div class="space-y-2">
         <div>
-          <p class="sm:text-[24px]">รูปแบบการแสดงผล</p>
-          <p class="text-sm text-white/60">ปรับแต่งวิธีการแสดงผลข้อความในแชท</p>
+          <p class="sm:text-[20px]">รูปแบบการแสดงผล</p>
+          <p class="text-xs sm:text-sm text-white/60">ปรับแต่งวิธีการแสดงผลข้อความในแชท</p>
         </div>
 
         <div class="flex items-center justify-between">
-          <div>
-            <p class="text-base sm:text-lg">ภาษา</p>  
-            <p class="text-sm text-white/60">เปลี่ยนภาษาแอปพลิเคชัน</p>
+          <div class="w-3/5">
+            <p class="text-sm sm:text-base">ภาษา</p>  
+            <p class="text-xs sm:text-sm text-white/60">เปลี่ยนภาษาแอปพลิเคชัน</p>
           </div>
           <ChangeLang />
         </div>
 
         <div class="flex items-center justify-between">
-          <div>
-            <p class="text-base sm:text-lg">การแสดงผลแบบอ่านนิยาย</p>  
+          <div class="w-3/5">
+            <p class="text-sm sm:text-base">การแสดงผลแบบอ่านนิยาย</p>  
             <p class="text-sm text-white/60">เปลี่ยนรูปแบบการแสดงผลจากแบบธรรมดาเป็นแบบอ่านนิยาย</p>
           </div>
           <USwitch :ui="{ 'thumb': 'bg-white'}" size="lg" />
         </div>
 
         <div class="flex items-center justify-between">
-          <div>
-            <p class="text-base sm:text-lg">พื้นหลังตัวละคร</p>  
-            <p class="text-sm text-white/60">ใช้รูปภาพตัวละครเป็นพื้นหลังแชท</p>
+          <div class="w-3/5">
+            <p class="text-sm sm:text-base">พื้นหลังตัวละคร</p>  
+            <p class="text-xs sm:text-sm text-white/60">ใช้รูปภาพตัวละครเป็นพื้นหลังแชท</p>
           </div>
           <USwitch :ui="{ 'thumb': 'bg-white'}" size="lg" />
         </div>
@@ -36,22 +36,22 @@
 
       <div class="space-y-2">
         <div>
-          <p class="sm:text-[24px]">ข้อความและบอลลูน</p>
-          <p class="text-sm text-white/60">ปรับแต่งขนาดตัวอักษรและความโปร่งใสของบอลลูนแชท</p>
+          <p class="sm:text-[20px]">ข้อความและบอลลูน</p>
+          <p class="text-xs sm:text-sm text-white/60">ปรับแต่งขนาดตัวอักษรและความโปร่งใสของบอลลูนแชท</p>
         </div>
 
-        <div>
+        <div class="space-y-2">
           <div>
-            <p class="text-base sm:text-lg">ขนาดตัวอักษรแชท</p>  
-            <p class="text-sm text-white/60">ปรับขนาดตัวอักษรในข้อความแชท</p>
+            <p class="text-sm sm:text-base">ขนาดตัวอักษรแชท</p>  
+            <p class="text-xs sm:text-sm text-white/60">ปรับขนาดตัวอักษรในข้อความแชท</p>
           </div>
           
-          <div class="flex gap-8 p-4 sm:p-8 bg-[#333E48] rounded-lg liquid-glass-cherri">
-            <p class="w-12 text-white text-[20px] font-bold">A<span class="text-[28px]">A</span></p>
+          <div class="flex items-center gap-8 p-4 sm:px-8 bg-[#333E48] rounded-lg liquid-glass-cherri">
+            <p class="hidden sm:block w-12 text-white text-[12px] sm:text-[20px] font-bold">A<span class="text-[18px] sm:text-[28px]">A</span></p>
             <div class="flex-1">
-              <p class="w-28 text-center">Text size</p>
-              <div class="flex items-center gap-8">
-                <p class="text-[20px] font-bold w-28 text-center">A</p>
+              <p class="hidden sm:block w-28 text-center">Text size</p>
+              <div class="flex items-center gap-2 sm:gap-8 h-[42px]">
+                <p class="text-[12px] sm:text-[20px] font-bold w-20 sm:w-28 text-center">A</p>
                 <USlider 
                   :ui="{ 
                     'range': 'bg-cherri-gradient',
@@ -60,24 +60,24 @@
                   }"
                   :default-value="50"
                 />
-                <p class="text-[28px] font-bold w-28 text-center">A</p>
+                <p class="text-[18px] sm:text-[28px] font-bold w-20 sm:w-28 text-center">A</p>
               </div>
             </div>
           </div>
         </div>
 
-        <div>
+        <div class="space-y-2">
           <div>
-            <p class="text-base sm:text-lg">ความโปร่งใสของบอลลูนแชท</p>  
-            <p class="text-sm text-white/60">ปรับความโปร่งใสของบอลลูนข้อความ</p>
+            <p class="text-sm sm:text-base">ความโปร่งใสของบอลลูนแชท</p>  
+            <p class="text-xs sm:text-sm text-white/60">ปรับความโปร่งใสของบอลลูนข้อความ</p>
           </div>
           
-          <div class="flex gap-8 p-4 sm:p-8 bg-[#333E48] rounded-lg liquid-glass-cherri">
-            <div class="w-12 h-12 bg-cherri/30 rounded-lg" />
+          <div class="flex items-center gap-8 p-4 sm:px-8 bg-[#333E48] rounded-lg liquid-glass-cherri">
+            <div class="hidden sm:block w-12 h-12 bg-cherri/30 rounded-lg" />
             <div class="flex-1">
-              <p class="w-28 text-center">ความโปร่งใส</p>
-              <div class="flex items-center gap-8">
-                <p class="w-28 text-center">โปร่งใส 0%</p>
+              <p class="hidden sm:block w-28 text-center">ความโปร่งใส</p>
+              <div class="flex items-center gap-2 sm:gap-8 h-[42px]">
+                <p class="text-sm sm:text-base w-20 sm:w-28 text-center">โปร่งใส 0%</p>
                 <USlider 
                   :ui="{ 
                     'range': 'bg-cherri-gradient',
@@ -86,7 +86,7 @@
                   }"
                   :default-value="50"
                 />
-                <p class="w-28 text-center">ทึบ 100%</p>
+                <p class="text-sm sm:text-base w-20 sm:w-28 text-center">ทึบ 100%</p>
               </div>
             </div>
           </div>
